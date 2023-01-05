b4af640843aeb66e44e89e12f7c94afe49446d22
Gamecraft400 <102668085+Gamecraft400@users.noreply.github.com>
2023-01-05 17:44:27 +0100

    Update PanelHautJeu.java

--- src/ihm/sectionJeu/PanelHautJeu.java
+++ src/ihm/sectionJeu/PanelHautJeu.java
@@ -1,6 +1,7 @@
 package ihm.sectionJeu;
 
 import main.Controleur;
+import metier.CarteWagon;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
@@ -9,6 +10,8 @@ import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import javax.swing.border.Border;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+
 import java.awt.Color;
 
 public class PanelHautJeu extends JPanel
@@ -17,6 +20,8 @@ public class PanelHautJeu extends JPanel
 
 	private JLabel lblTour;
 	private JLabel lblProfil;
+	private JPanel panelDispoCarte;
+	private int carteDispoMarchée;
 
 	public PanelHautJeu(Controleur ctrl)
 	{
@@ -30,11 +35,16 @@ public class PanelHautJeu extends JPanel
 		JPanel panelProfilJoueur = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 50));
 		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
 		this.lblTour = new JLabel("C'est au tour de ");
+		this.panelDispoCarte = new JPanel(new GridLayout(1,5));
+		this.carteDispoMarchée = 0;
 
 		/**
 		 * Positionnement des composants
 		 */
 
+		 //this.majCarteMarcher();
+		
+		/*
 		for(int i = 0; i < this.ctrl.getNbJoueurMax(); i++)
 		{
 			panelTourJoueur.add(new JLabel("Joueur " + (i + 1)));
@@ -42,15 +52,30 @@ public class PanelHautJeu extends JPanel
 			lblProfil.setBorder(border);
 			panelProfilJoueur.add(lblProfil);
 		}
+		*/
 
 		panelTourJoueur.setBorder(border);
 		panelProfilJoueur.setBorder(border);
 
-		this.add(panelTourJoueur, BorderLayout.EAST);
+		this.add(panelDispoCarte, BorderLayout.EAST);
 		this.add(panelProfilJoueur);
 
 		/**
 		 * Activation des composants
 		 */
+	}
+
+	public void majCarteMarcher()
+	{
+		
+		while(this.carteDispoMarchée < 5 /*&& this.ctrl.get().size() > 0*/)
+		{
+			JPanel tmp = new JPanel();
+			CarteWagon carteWagon = this.ctrl.getLstCarteWagon().remove((int)(Math.random()*this.ctrl.getLstCarteWagon().size()));
+			System.out.println( "n°"+this.carteDispoMarchée + carteWagon);
+			tmp.setBackground(carteWagon.getColor());
+			this.panelDispoCarte.add(tmp);
+			this.carteDispoMarchée++;
+		}
 	}
 }
