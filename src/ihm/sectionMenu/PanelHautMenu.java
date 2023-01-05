5e1d254d73465b871e79803a07541b2a33243caa
EstebanBreaHell <esteban.breahell@gmail.com>
2023-01-05 16:55:36 +0100

    Truc qui récup les ficchier

--- src/ihm/sectionMenu/PanelHautMenu.java
+++ src/ihm/sectionMenu/PanelHautMenu.java
@@ -5,7 +5,9 @@ import main.Controleur;
 import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JLabel;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 
 import java.awt.event.ActionEvent;
@@ -20,13 +22,36 @@ import java.awt.Font;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import java.awt.*;
+import java.awt.event.*;
+
+
+import java.util.ArrayList;
+
+
+import java.awt.event.MouseEvent;
+import javax.swing.filechooser.FileSystemView;
+
+
+import metier.Arete;
+import metier.Noeud;
+
 public class PanelHautMenu extends JPanel implements ActionListener
 {
 	private Controleur ctrl;
 
+	private JDialog jdImporteImageManquante;
+
 	private JButton btnImport;
+	private JButton btnImportImage;
 	private JPanel panelInformation;
 	private JLabel lblInformationMappe;
+
+	private File FileImagenew;
+
+	private String cheminFichier;
+
+	private boolean validFichier;
 
 	public PanelHautMenu(Controleur ctrl)
 	{
@@ -36,7 +61,10 @@ public class PanelHautMenu extends JPanel implements ActionListener
 
 		this.ctrl = ctrl; 
 		this.setLayout(new BorderLayout());
-		
+
+
+		this.validFichier = false;
+
 		JLabel lblTitre 	= new JLabel("Les aventuriers du rail");
 
 		JPanel panelTitre 	= new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 40));
@@ -81,6 +109,7 @@ public class PanelHautMenu extends JPanel implements ActionListener
 	
 	public void actionPerformed(ActionEvent e)
 	{
+		
 		if(e.getSource() == this.btnImport)
 		{
 			FileNameExtensionFilter filtre = new FileNameExtensionFilter("Format XML", "xml");
@@ -88,7 +117,7 @@ public class PanelHautMenu extends JPanel implements ActionListener
 			
 			jFileChooser.setAcceptAllFileFilterUsed(false);
 			jFileChooser.setFileFilter(filtre);
-
+			
 			int res = jFileChooser.showOpenDialog(null);
 
 			if(res == JFileChooser.APPROVE_OPTION)
@@ -103,6 +132,8 @@ public class PanelHautMenu extends JPanel implements ActionListener
 						this.ctrl.lireXml("donnee/xml/"+file.getName());
 						this.panelInformation.setLayout(new GridLayout(10, 0,0,5));
 
+						
+
 						this.lblInformationMappe.setText(lblVide.getText());
 						this.panelInformation.add(new JLabel("Nombre de joueurs maximum  : " 						+ this.ctrl.getNbJoueurMax()			, JLabel.CENTER));
 						this.panelInformation.add(new JLabel("Nombre de couleur / joueur : "      					+ this.ctrl.getLstCouleurJoueur().size(), JLabel.CENTER));
@@ -114,10 +145,78 @@ public class PanelHautMenu extends JPanel implements ActionListener
 						this.panelInformation.add(new JLabel("Nombre de wagons restants pour arrêter la partie : " 	+ this.ctrl.getNbWagonFinPartie()		, JLabel.CENTER));
 						this.panelInformation.add(new JLabel("Nombre de wagons / joueurs : " 						+ this.ctrl.getNbWagonDebutPartie()		, JLabel.CENTER));
 				
-						this.ctrl.getPanelCentreMenu().setEnabled(true);
+
+						String nomImage = this.ctrl.getNomImage();
+						this.FileImagenew =new File(nomImage);
+
+						while(!FileImagenew.exists())
+						{
+							System.out.println("Sheeeeeesh1");
+							try{
+								if(!this.validFichier)
+								{
+									this.jdImporteImageManquante = new JDialog();
+									this.btnImportImage = new JButton("Importé le fichier manquant");
+									this.btnImportImage.addActionListener(this);
+									this.jdImporteImageManquante.setTitle("Erreur");
+									this.jdImporteImageManquante.setSize(300, 100);
+									this.jdImporteImageManquante.setLocationRelativeTo(null);
+									this.jdImporteImageManquante.setModal(true);
+									this.jdImporteImageManquante.setResizable(false);
+									this.jdImporteImageManquante.setLayout(new BorderLayout());
+									this.jdImporteImageManquante.add(btnImportImage, BorderLayout.SOUTH);
+									this.jdImporteImageManquante.add(new JLabel("L'image " + nomImage +  " n'existe pas, veuillez la placer dans le dossier image"), BorderLayout.CENTER);
+									//FileNameExtensionFilter filtreImage = new FileNameExtensionFilter("Format XML", "png")
+									this.jdImporteImageManquante.setVisible(true);
+									this.validFichier = true;
+									
+								}
+								else
+								{
+									this.jdImporteImageManquante.dispose();
+									this.FileImagenew =new File(this.ctrl.getNomImage());
+								}
+								
+
+							}
+
+
+							catch(Exception e1){e1.printStackTrace();}
+						}
 					}
-					} catch (IOException e1) {e1.printStackTrace();}
+
+						
+
+						
+				} catch (IOException e1) {e1.printStackTrace();}
 			}
 		}
+
+		if(e.getSource() == this.btnImportImage)
+		{
+			JFileChooser jFileChooserRemplacement = new JFileChooser(new File("donnee/xml"));
+			int res2 = jFileChooserRemplacement.showOpenDialog(null);
+
+			
+			if(res2 == JFileChooser.APPROVE_OPTION)
+			{
+				System.out.print("Sheeeesh2");
+				File fileRemplacement = jFileChooserRemplacement.getSelectedFile();									
+				try {
+					Files.copy(fileRemplacement.toPath(), Paths.get("importe/"	+ fileRemplacement.getName()));
+				} catch (IOException e2) { e2.printStackTrace();}																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																										
+				this.cheminFichier = this.ctrl.getNomImage();
+
+				this.validFichier = this.ctrl.getNomImage() == cheminFichier;
+				this.jdImporteImageManquante.dispose();
+				this.FileImagenew =new File(cheminFichier);
+				//this.jdImporteImageManquante.dispose();
+			}
+
+		}
+
+		
+
+		
 	}
 }
