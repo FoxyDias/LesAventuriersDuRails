de01427542cf3e0306598ef8beb953fa5c24764c
AntoineDardanne <antoine.dardanne25@gmail.com>
2023-01-09 15:28:20 +0100

    Ajout panel droite

---
+++ src/ihm/sectionJeu/PanelDroiteJeu.java
@@ -0,0 +1,79 @@
+package ihm.sectionJeu;
+
+import main.Controleur;
+import metier.CarteWagon;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+import java.awt.GridLayout;
+import java.awt.event.*;
+import java.util.Collections;
+
+public class PanelDroiteJeu extends JPanel
+{
+	private Controleur ctrl;
+
+	public PanelDroiteJeu(Controleur ctrl)
+	{
+		this.ctrl = ctrl;
+		/**
+		 * Création des composants
+		 */
+		this.setLayout(new GridLayout(5,1, 5, 5 ));
+		this.setPreferredSize(new java.awt.Dimension(100, 0));
+		
+
+		/**
+		 * Positionnement des composants
+		 */
+		for(int index = 0; index < 5; index++)
+		{
+			Collections.shuffle(this.ctrl.getLstCarteWagon());
+			this.add(new PanelPiocheMarcher(this.ctrl.getLstCarteWagon().remove(0)));
+			
+			//this.add(new PanelPiocheMarcher(this.ctrl.getLstCarteWagon().remove(0)));
+		}
+
+		/**
+		 * Activation des composants
+		 */
+	}
+
+	public class PanelPiocheMarcher extends JPanel implements ActionListener
+	{
+		private CarteWagon carteWagon;
+		private JButton btnPrendCarte;
+
+		public PanelPiocheMarcher(CarteWagon carteWagon)
+		{
+			this.carteWagon = carteWagon;
+
+			this.btnPrendCarte = new JButton("prendre carte");
+
+			this.add(this.btnPrendCarte);
+			this.setBackground(this.carteWagon.getColor());
+			this.btnPrendCarte.addActionListener(this);
+		}
+
+		public void majMarcher(CarteWagon carteWagon)
+		{
+			this.carteWagon = carteWagon;
+			this.setBackground(this.carteWagon.getColor());
+			this.repaint();
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			if(e.getSource() == this.btnPrendCarte)
+			{
+				PanelDroiteJeu.this.ctrl.getEstJoueurCourant().ajouterCarteWagon(this.carteWagon);
+				this.majMarcher(PanelDroiteJeu.this.ctrl.getLstCarteWagon().remove(0));
+			}
+		}
+
+	}
+	
+}
+
