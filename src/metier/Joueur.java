9e42341133dbb80bf27451d12d9f3368c6f82388
EstebanBreaHell <esteban.breahell@gmail.com>
2023-01-10 10:55:15 +0100

    Point ishalla

--- src/metier/Joueur.java
+++ src/metier/Joueur.java
@@ -2,6 +2,7 @@ package metier;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import ihm.sectionJeu.PanelHautJeu;
 
@@ -44,24 +45,10 @@ public class Joueur {
      * Renvoi le nombre de points des chemins du joueur
      */
     public int getNbPointsChemin() {
+        this.nbPoints = 0;
         //Les points d'une arête sont calculés par rapport aux nombres de wagons nécessaires pour la parcourire
         for (Arete a : this.lstArete){
-            if( this.alCheminsPtsCpts.size() != 0){
-                for(Arete b : this.alCheminsPtsCpts){
-                    /* Si l'arête a déjà étée comptabilisée on augmente pas le nbPoints */
-                    if(a.equals(b))
-                        return nbPoints;   
-                    else{
-                        /* Sinon on augmente le nbPoints et on l'ajoute dans l'arrayList des arêtes déjà comptabilisées */
-                        this.nbPoints += metier.getPointsTailleAretes(a.getWagon());
-                        this.alCheminsPtsCpts.add(a);
-                    }
-                }
-            }else{
-                /* Pour le premier cas si aucun joueurs n'a d'arêtes */
-                this.nbPoints += this.metier.getPointsTailleAretes(a.getWagon());
-                this.alCheminsPtsCpts.add(a);
-            }
+            this.nbPoints+= metier.getPointsTailleAretes(a.getWagon());
         }
         return this.nbPoints;
     }
